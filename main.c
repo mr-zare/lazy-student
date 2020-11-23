3aedd4f1a5c17e66241f98dd127d584a81d5eca6
mr.z <mo.erfan1379@gmail.com>
2020-11-23 11:03:57 +0330

    sen

--- main.c
+++ main.c
@@ -2,18 +2,10 @@
 
 int main()
 {
-	int a;
-	float m=0,b;
-	scanf("%d",&a);
-	//a:tedad emtehanat v b:nomre anha m:mopadel fard ast 
-	for (int  i = 0; i < a; i++)
-	{
-		scanf("%f",&b);
-		m=m+b;
-	}
-	m=m/a;
-	printf("%f",m);
-	
-
+	int s,hour,rooz;
+	scanf("%d",&hour);
+	s=(hour/(365*24));
+	rooz=(hour%(365*24)/24);
+	printf("%d %d",s,rooz);
 	return (0);
 }
